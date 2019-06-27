ca4a958f09a7ef1cfb11992d7e519d3058cc3ea2
naidu95 <naidu.kvana@gmail.com>
2019-06-27 18:03:55 +0530

    finsal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,5 +11,6 @@ export class AppComponent implements OnInit{
   constructor(private db: AngularFireDatabase){}
   ngOnInit(){
     console.log(this.db.list('/users').valueChanges().subscribe(users=>{console.log(users)}));
+    
   }
 }
--- src/app/ngrx/create/create.component.ts
+++ src/app/ngrx/create/create.component.ts
@@ -4,7 +4,8 @@ import { AppState } from '../../app.state';
 import { Tutorial } from '../../models/tutorial.model'
 import * as TutorialActions from '../../actions/tutorial.actions';
 import { Observable } from 'rxjs';
-
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -12,15 +13,20 @@ import { Observable } from 'rxjs';
 })
 export class CreateComponent implements OnInit {
   tutorials: Observable<Tutorial[]>;
-  
-  constructor(private store: Store<AppState>) {
+  userId:any;
+  constructor(private store: Store<AppState>,public auth:AngularFireAuth) {
     this.tutorials = store.select('tutorial');
     
+    this.userId=firebase.auth().currentUser.uid;
   }
   delTutorial(index) {
     this.store.dispatch(new TutorialActions.RemoveTutorial(index) )
   }
   addTutorial(name, url) {
+    
+    firebase.database().ref('users/'+this.userId).push({
+      username: "Hai users",
+    });
     this.store.dispatch(new TutorialActions.AddTutorial({name: name, url: url}) )
   }
 
